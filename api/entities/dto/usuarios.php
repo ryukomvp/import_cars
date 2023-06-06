bdf7d4c89c80a94433484cad0cd3e3984c4bd838
ryukomvp <daniel123hernandez15@gmail.com>
2023-06-06 12:09:00 -0600

    DTO de usuarios

--- api/entities/dto/usuarios.php
+++ api/entities/dto/usuarios.php
@@ -8,11 +8,12 @@ class Usuario extends UsuarioQueries
 {
     // Declaración de atributos (propiedades).
     protected $id = null;
-    protected $nombres = null;
-    protected $apellidos = null;
-    protected $correo = null;
-    protected $alias = null;
+    protected $nombre = null;
     protected $clave = null;
+    protected $pin = null;
+    protected $tipo = null;
+    protected $empleado = null;
+    protected $estado = null;
 
     /*
     *   Métodos para validar y asignar valores de los atributos.
@@ -27,40 +28,10 @@ class Usuario extends UsuarioQueries
         }
     }
 
-    public function setNombres($value)
+    public function setNombre($value)
     {
         if (Validator::validateAlphabetic($value, 1, 50)) {
-            $this->nombres = $value;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function setApellidos($value)
-    {
-        if (Validator::validateAlphabetic($value, 1, 50)) {
-            $this->apellidos = $value;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function setCorreo($value)
-    {
-        if (Validator::validateEmail($value)) {
-            $this->correo = $value;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function setAlias($value)
-    {
-        if (Validator::validateAlphanumeric($value, 1, 50)) {
-            $this->alias = $value;
+            $this->nombre = $value;
             return true;
         } else {
             return false;
@@ -77,6 +48,46 @@ class Usuario extends UsuarioQueries
         }
     }
 
+    public function setPin($value)
+    {
+        if (Validator::validateString($value, 1, 10)) {
+            $this->pin = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setTipo($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->tipo = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setEmpleado($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->empleado = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setEstado($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->estado = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /*
     *   Métodos para obtener valores de los atributos.
     */
@@ -85,28 +96,33 @@ class Usuario extends UsuarioQueries
         return $this->id;
     }
 
-    public function getNombres()
+    public function getNombre()
     {
-        return $this->nombres;
-    }
-
-    public function getApellidos()
-    {
-        return $this->apellidos;
-    }
-
-    public function getCorreo()
-    {
-        return $this->correo;
-    }
-
-    public function getAlias()
-    {
-        return $this->alias;
+        return $this->nombre;
     }
 
     public function getClave()
     {
         return $this->clave;
+    }
+
+    public function getPin()
+    {
+        return $this->pin;
+    }
+
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    public function getEmpleado()
+    {
+        return $this->empleado;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
     }
 }
