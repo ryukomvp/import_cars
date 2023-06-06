f666c2207d33478135a5a631aada7142846db56d
Gregfuxito <82457539+Gregfuxito@users.noreply.github.com>
2023-06-06 12:06:46 -0600

    Si

---
+++ api/business/marcas.php
@@ -0,0 +1,100 @@
+<?php
+require_once('../../entities/dto/marcas.php');
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if(isset($_GET['action'])){
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $marca = new Marca;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'message' => null, 'exception' => null, 'dataset' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+    if(isset($_SESSION['id_usuario'])){
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch($_GET['action']) {
+            case 'readAll':
+                if ($result['dataset'] = $marca->readAll()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen '.count($result['dataset']).' registros';
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay datos registrados';
+                }
+                break;
+            case 'search':
+                $_POST = Validator::validateForm($_POST);
+                   if ($_POST['search'] == '') {
+                       $result['exception'] = 'Ingrese un valor para buscar';
+                } elseif ($result['dataset'] = $marca->searchRows($_POST['search'])) {
+                       $result['status'] = 1;
+                    $result['message'] = 'Existen '.count($result['dataset']).' coincidencias';
+                } elseif (Database::getException()) {
+                       $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay coincidencias';
+                }
+                 break;
+            case 'create':
+                $_POST = Validator::validateForm($_POST);
+                if (!$marca->setMarca($_POST['nombre'])){
+                    $result['exception'] = 'Nombre incorrecto'; 
+                } elseif ($marca->createRow()){
+                    $result['status'] = 1;
+                    $result['message'] = 'Marca creada correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;   
+            case 'readOne':
+                if (!$marca->setId($_POST['id_marca'])) {
+                        $result['exception'] = 'Marca incorrecta';
+                } elseif ($result['dataset'] = $marca->readOne()) {
+                       $result['status'] = 1;
+                } elseif (Database::getException()) {
+                       $result['exception'] = Database::getException();
+                } else {
+                       $result['exception'] = 'Marca inexistente';
+                }
+                break;
+            case 'update':
+                $_POST = Validator::validateForm($_POST);
+               if (!$marca->setId($_POST['id'])) {
+                    $result['exception'] = 'Marca incorrecta';
+                } elseif (!$data = $marca->readOne()) {
+                    $result['exception'] = 'Categoría inexistente';
+                } elseif (!$marca->setNombre($_POST['nombre'])) {
+                     $result['exception'] = 'Nombre incorrecto';
+                } elseif ($marca->updateRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Marca modificada correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'delete':
+                if (!$marca->setId($_POST['id_marca'])) {
+                    $result['exception'] = 'Marca incorrecta';
+                } elseif (!$data = $marca->readOne()) {
+                    $result['exception'] = 'Marca inexistente';
+                } elseif ($marca->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Marca eliminada correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            default:
+                    $result['exception'] = 'Acción no disponible dentro de la sesión';
+                break;
+        }
+        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+        header('content-type: application/json; charset=utf-8');
+        // Se imprime el resultado en formato JSON y se retorna al controlador.
+        print(json_encode($result));
+    } else {
+        print(json_encode('Acceso denegado'));
+    }
+} else {
+    print(json_encode('Recurso no disponible'));
+}
---
+++ controllers/marcas.js
@@ -0,0 +1,148 @@
+const MARCA_API = 'business/dashboard/marcas.php';
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('search-form');
+// Constante para establecer el formulario de guardar.
+const SAVE_FORM = document.getElementById('save-form');
+// Constante para establecer el título de la modal.
+const MODAL_TITLE = document.getElementById('modal-title');
+// Constantes para establecer el contenido de la tabla.
+const TBODY_ROWS = document.getElementById('tbody-rows');
+const RECORDS = document.getElementById('records');
+const SAVE_MODAL = new bootstrap.Modal(document.getElementById('save-modal'));
+// Método manejador de eventos para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para llenar la tabla con los registros disponibles.
+    fillTable();
+});
+
+// Método manejador de eventos para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    fillTable(FORM);
+});
+
+// Método manejador de eventos para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (document.getElementById('id').value) ? action = 'update' : action = 'create';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const JSON = await dataFetch(MARCA_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, JSON.message, true);
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+});
+
+async function fillTable(form = null) {
+    // Se inicializa el contenido de la tabla.
+    TBODY_ROWS.innerHTML = '';
+    RECORDS.textContent = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'search' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const JSON = await dataFetch(MARCA_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se recorre el conjunto de registros fila por fila.
+        JSON.dataset.forEach(row => {
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TBODY_ROWS.innerHTML += `
+                <tr>
+                    <td>${row.id_marca}</td>
+                    <td>${row.nombre_marca}</td>
+                    <td>
+                    <!--Botones de acciones-->
+                    <button data-modal-target="modal_update_category1" data-modal-toggle="modal_update_category1" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button"><img src="https://img.icons8.com/ios/30/FFFFFF/synchronize.png" />
+                    <span class="sr-only">Cerrar ventana</span>
+                    </button>
+                    <button data-modal-target="delete-modal1" data-modal-toggle="delete-modal1" class="md:w-auto text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button"><img src="https://img.icons8.com/ios/30/FFFFFF/delete--v1.png" />
+                    </button>
+                    </td>
+                </tr>
+            `;
+        });
+        // Se muestra un mensaje de acuerdo con el resultado.
+        RECORDS.textContent = JSON.message;
+    } else {
+        sweetAlert(4, JSON.exception, true);
+    }
+}
+
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+function openCreate() {
+    // Se abre la caja de diálogo que contiene el formulario.
+    SAVE_MODAL.show();
+    // Se asigna título a la caja de diálogo.
+    MODAL_TITLE.textContent = 'Crear marca';
+}
+
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+async function openUpdate(id) {
+    // Se define una constante tipo objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('id_marca', id);
+    // Petición para obtener los datos del registro solicitado.
+    const JSON = await dataFetch(MARCA_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se abre la caja de diálogo que contiene el formulario.
+        SAVE_MODAL.show();
+        // Se restauran los elementos del formulario.
+        SAVE_FORM.reset();
+        // Se asigna título para la caja de diálogo.
+        MODAL_TITLE.textContent = 'Actualizar marca';
+        // Se inicializan los campos del formulario.
+        document.getElementById('id').value = JSON.dataset.id_marca;
+        document.getElementById('nombre').value = JSON.dataset.nombre_marca;
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+}
+
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+async function openDelete(id) {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar la marca de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('id_marca', id);
+        // Petición para eliminar el registro seleccionado.
+        const JSON = await dataFetch(MARCA_API, 'delete', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (JSON.status) {
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+            // Se muestra un mensaje de éxito.
+            sweetAlert(1, JSON.message, true);
+        } else {
+            sweetAlert(2, JSON.exception, false);
+        }
+    }
+}
