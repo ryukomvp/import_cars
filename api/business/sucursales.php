d20db527f171f37620ef7a3211b7c10c47070257
Theguishe <20210120@ricaldone.edu.sv>
2023-06-13 11:51:24 -0600

    sucursales

--- api/business/sucursales.php
+++ api/business/sucursales.php
@@ -10,7 +10,7 @@ if(isset(%$_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'exception' => null, 'dataset' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if (isset($_SESSION['id_usuario'])) {
+    if (isset($_SESSION['idusuario'])) {
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch ($_GET['action']) {
             case 'readAll':
@@ -78,11 +78,35 @@ if(isset(%$_GET['action'])) {
                     $result['exception'] = 'Correo incorrecto';
                 } elseif (!$sucursal->setDireccion($_POST['direccion'])) {
                     $result['exception'] = 'Direccion incorrecta';
+                } elseif ($sucursal->updateRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Sucursal modificada correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
                 }
                 break;
             case 'delete':
-                
+                if (!$sucursal->setId($_POST['idsucursal'])) {
+                    $result['exception'] = 'Sucursal incorrecta';
+                } elseif (!$data = $sucursal->readOne()) {
+                    $result['exception'] = 'Sucursal inexistente';
+                } elseif ($sucursal->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Sucursal eliminada correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
                 break;
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
         }
+        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+        header('content-type: application/json; charset=utf-8');
+        // Se imprime el resultado en formato JSON y se retorna al controlador.
+        print(json_encode($result));
+    } else {
+        print(json_encode('Acceso denegado'));
     }
+} else {
+    print(json_encode('Recurso no disponible'));
 }
---
+++ controllers/sucursales.js
@@ -0,0 +1,191 @@
+// Constante para completar la ruta de la API.
+const CATEGORIA_API = 'business/sucursales.php';
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('search-form');
+// Constante para establecer el formulario de guardar.
+const SAVE_FORM = document.getElementById('save-form');
+// Constante para establecer el título de la modal.
+const MODAL_TITLE = document.getElementById('modal-title');
+// Constantes para establecer el contenido de la tabla.
+const TBODY_ROWS = document.getElementById('tbody-rows');
+const RECORDS = document.getElementById('records');
+
+// Se inicializa el componente Modal para que funcionen las cajas de diálogo.
+// Constante para establecer la modal de guardar.
+const SAVE_MODAL = new Modal(document.getElementById(''));
+
+// Método manejador de eventos para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para llenar la tabla con los registros disponibles.
+    fillTable();
+});
+
+// Método manejador de eventos para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    fillTable(FORM);
+});
+
+// Método manejador de eventos para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (document.getElementById('id').value) ? action = 'update' : action = 'create';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const JSON = await dataFetch(CATEGORIA_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+        // Se cierra la caja de diálogo.
+        SAVE_MODAL.hide();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, JSON.message, true);
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+});
+
+/*
+*   Función asíncrona para llenar la tabla con los registros disponibles.
+*   Parámetros: form (objeto opcional con los datos de búsqueda).
+*   Retorno: ninguno.
+*/
+async function fillTable(form = null) {
+    // Se inicializa el contenido de la tabla.
+    TBODY_ROWS.innerHTML = '';
+    RECORDS.textContent = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'search' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const JSON = await dataFetch(CATEGORIA_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se recorre el conjunto de registros fila por fila.
+        JSON.dataset.forEach(row => {
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TBODY_ROWS.innerHTML += `
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 dark:hover:bg-gray-600">
+                    <td class="px-6 py-4">
+                        ${row.nombre}
+                    </td>
+                    <td class="px-6 py-4">
+                        ${row.telefono}
+                    </td>
+                    <td class="px-6 py-4">
+                        ${row.correo}
+                    </td>
+                    <td class="px-6 py-4">
+                        ${row.direccion}
+                    </td>
+                    <td class="px-6 py-4">
+                        <!--contenedor de los controles de actualizar y eliminar-->
+                        <div class="grid gap-2 m-0">
+                            <!--botón de actualizar-->
+                            <div>
+                                <button data-modal-target="modal_update_branch-office1"
+                                    data-modal-toggle="modal_update_branch-office1"
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    type="button">
+                                    <img src="https://img.icons8.com/ios/30/FFFFFF/synchronize.png" />
+                                </button>
+                            </div>
+                            <!--botón de eliminar registro-->
+                            <div>
+                                <button data-modal-target="delete-modal1" data-modal-toggle="delete-modal1"
+                                    class="md:w-auto text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    type="button">
+                                    <img src="https://img.icons8.com/ios/30/FFFFFF/delete--v1.png" />
+                                </button>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            `;
+        });
+        // Se muestra un mensaje de acuerdo con el resultado.
+        RECORDS.textContent = JSON.message;
+    } else {
+        sweetAlert(4, JSON.exception, true);
+    }
+}
+
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+function openCreate() {
+    // Se abre la caja de diálogo que contiene el formulario.
+    SAVE_MODAL.show();
+    // Se restauran los elementos del formulario.
+    SAVE_FORM.reset();
+    // Se asigna título a la caja de diálogo.
+    MODAL_TITLE.textContent = 'Crear sucursal';
+}
+
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+async function openUpdate(id) {
+    // Se define una constante tipo objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('idsucursal', id);
+    // Petición para obtener los datos del registro solicitado.
+    const JSON = await dataFetch(CATEGORIA_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se abre la caja de diálogo que contiene el formulario.
+        SAVE_MODAL.show();
+        // Se restauran los elementos del formulario.
+        SAVE_FORM.reset();
+        // Se asigna título para la caja de diálogo.
+        MODAL_TITLE.textContent = 'Actualizar sucursal';
+        // Se establece el campo de archivo como opcional.
+        document.getElementById('archivo').required = false;
+        // Se inicializan los campos del formulario.
+        document.getElementById('id').value = JSON.dataset.idsucursal;
+        document.getElementById('nombre').value = JSON.dataset.nombre;
+        document.getElementById('telefono').value = JSON.dataset.telefono;
+        document.getElementById('correo').value = JSON.dataset.correo;
+        document.getElementById('direccion').value = JSON.dataset.direccion;
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+}
+
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+async function openDelete(id) {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar la sucursal de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idsucursal', id);
+        // Petición para eliminar el registro seleccionado.
+        const JSON = await dataFetch(CATEGORIA_API, 'delete', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (JSON.status) {
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+            // Se muestra un mensaje de éxito.
+            sweetAlert(1, JSON.message, true);
+        } else {
+            sweetAlert(2, JSON.exception, false);
+        }
+    }
+}
