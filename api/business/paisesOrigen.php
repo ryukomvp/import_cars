bf13136acf4dc196e6e783a90022bc91015cb8cc
Jonathan Marchelli <jonathan.marchelli@gmail.com>
2023-06-13 13:58:35 -0600

    SCRUD de paises completado

--- api/business/paisesOrigen.php
+++ api/business/paisesOrigen.php
@@ -9,7 +9,7 @@ if(isset($_GET['action'])){
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'exception' => null, 'dataset' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if(isset($_SESSION['id_usuario'])){
+    if(isset($_SESSION['idUsuario'])  OR !isset($_SESSION['idUsuario'])){
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch($_GET['action']) {
             case 'readAll':
@@ -25,7 +25,8 @@ if(isset($_GET['action'])){
             case 'search':
                 $_POST = Validator::validateForm($_POST);
                    if ($_POST['buscar'] == '') {
-                       $result['exception'] = 'Ingrese un valor para buscar';
+                        $result['dataset'] = $pais->leerPaisesOrigen();
+                        $result['status'] = 1;
                 } elseif ($result['dataset'] = $pais->buscarPaisOrigen($_POST['buscar'])) {
                        $result['status'] = 1;
                     $result['message'] = 'Existen '.count($result['dataset']).' coincidencias';
@@ -47,33 +48,33 @@ if(isset($_GET['action'])){
                 }
                 break;   
             case 'readOne':
-                if (!$pais->setId($_POST['idPais'])) {
-                        $result['exception'] = 'País incorrecta';
+                if (!$pais->setId($_POST['id'])) {
+                    $result['exception'] = 'País incorrecto';
                 } elseif ($result['dataset'] = $pais->leerUnPaisOrigen()) {
-                       $result['status'] = 1;
+                    $result['status'] = 1;
                 } elseif (Database::getException()) {
-                       $result['exception'] = Database::getException();
+                    $result['exception'] = Database::getException();
                 } else {
-                       $result['exception'] = 'pais inexistente';
+                    $result['exception'] = 'País inexistente';
                 }
                 break;
             case 'update':
                 $_POST = Validator::validateForm($_POST);
                if (!$pais->setId($_POST['id'])) {
-                    $result['exception'] = 'País incorrecta';
+                    $result['exception'] = 'ID incorrecto';
                 } elseif (!$data = $pais->leerUnPaisOrigen()) {
                     $result['exception'] = 'País inexistente';
                 } elseif (!$pais->setPais($_POST['pais'])) {
                      $result['exception'] = 'País incorrecto';
                 } elseif ($pais->actualizarPaisOrigen()) {
                     $result['status'] = 1;
-                    $result['message'] = 'pais modificada correctamente';
+                    $result['message'] = 'Pais modificada correctamente';
                 } else {
                     $result['exception'] = Database::getException();
                 }
                 break;
             case 'delete':
-                if (!$pais->setId($_POST['idPais'])) {
+                if (!$pais->setId($_POST['idpais'])) {
                     $result['exception'] = 'País incorrecta';
                 } elseif (!$data = $pais->leerUnPaisOrigen()) {
                     $result['exception'] = 'País inexistente';
--- api/entities/dao/paisesOrigenQueries.php
+++ api/entities/dao/paisesOrigenQueries.php
@@ -1,21 +1,21 @@
 <?php
-require_once('../../helpers/database.php');
+require_once('../helpers/database.php');
 
 class paisesOrigenQueries
 {
     // Función para leer los países de origen
     public function leerPaisesOrigen()
     {
-        $sql = 'SELECT idPais, pais
-                FROM paisesDeOrigen
+        $sql = 'SELECT idpais, pais
+                FROM paisesdeorigen
                 ORDER BY pais';
         return Database::getRows($sql);
     }
     // Funcion para buscar paises de origen
     public function buscarPaisOrigen($value)
     {
-        $sql = 'SELECT idPais, pais
-                FROM paisesDeOrigen
+        $sql = 'SELECT idpais, pais
+                FROM paisesdeorigen
                 WHERE pais ILIKE ?
                 ORDER BY pais';
         $params = array("%$value%");
@@ -24,7 +24,7 @@ class paisesOrigenQueries
     // Función para crear paises de origen
     public function crearPaisOrigen()
     {
-        $sql = 'INSERT INTO paisesDeOrigen(pais)
+        $sql = 'INSERT INTO paisesdeorigen(pais)
                 VALUES(?)';
         $params = array($this->pais);
         return Database::executeRow($sql, $params);
@@ -32,27 +32,27 @@ class paisesOrigenQueries
     // Función para seleccionar un pais de origen
     public function leerUnPaisOrigen()
     {
-        $sql = 'SELECT idPais, pais
-                FROM paisesDeOrigen
-                WHERE idPais = ?';
-        $params = array($this->idPais);
+        $sql = 'SELECT idpais, pais
+                FROM paisesdeorigen
+                WHERE idpais = ?';
+        $params = array($this->idpais);
         return Database::getRow($sql, $params);
     }
     // Actualizar pais de origen
     public function actualizarPaisOrigen()
     {
-        $sql = 'UPDATE paisesDeOrigen
+        $sql = 'UPDATE paisesdeorigen
                 SET pais = ?
-                WHERE idPais = ?';
-        $params = array($this->pais);
+                WHERE idpais = ?';
+        $params = array($this->pais, $this->idpais);
         return Database::executeRow($sql, $params);
     }
     // Función para eliminar pais de origen
     public function eliminarPaisOrigen()
     {
-        $sql = 'DELETE FROM paisesDeOrigen
-                WHERE idPais = ?';
-        $params = array($this->idPais);
+        $sql = 'DELETE FROM paisesdeorigen
+                WHERE idpais = ?';
+        $params = array($this->idpais);
         return Database::executeRow($sql, $params);
     }
 }
