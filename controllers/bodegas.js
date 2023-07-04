16dff653646d00b640e14582e3f1d45c98d89631
ryukomvp <daniel123hernandez15@gmail.com>
2023-07-04 09:58:54 -0600

    Estandarización en SCRUD de bodegas
    Estandarización finalizada en el SCRUD de bodegas

--- controllers/bodegas.js
+++ controllers/bodegas.js
@@ -87,7 +87,6 @@ async function cargarRegistros(form = null) {
                     </td>
                     <td class="px-6 py-4">
                         <button onclick="openUpdate(${row.idbodega})"
-                            data-modal-toggle="modal_update_branch-office1"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                             type="button">
                             <img src="https://img.icons8.com/ios/30/FFFFFF/synchronize.png" />
