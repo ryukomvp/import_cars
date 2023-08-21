fff6ca5400fae0915411dda75ba2ef77eb2f111f
ryukomvp <daniel123hernandez15@gmail.com>
2023-08-21 15:26:14 -0600

    Reporte de productos por marca finalizado :D

--- api/reports/productosPorMarca.php
+++ api/reports/productosPorMarca.php
@@ -15,7 +15,7 @@ if (isset($_GET['idmarca'])) {
         // Se verifica si la categoría existe, de lo contrario se muestra un mensaje.
         if ($rowMarcas = $marcas->leerUnRegistro()) {
             // Se inicia el reporte con el encabezado del documento.
-            $pdf->startReport('Productos de la marca ' . $rowMarcas['marca']);
+            $pdf->startReport('Productos de la marca: ' . $rowMarcas['marca']);
             // Se verifica si existen registros para mostrar, de lo contrario se imprime un mensaje.
             if ($dataProductos = $marcas->productosMarca()) {
                 // Se establece un color de relleno para los encabezados.
@@ -23,31 +23,31 @@ if (isset($_GET['idmarca'])) {
                 // Se establece la fuente para los encabezados.
                 $pdf->setFont('Times', 'B', 11);
                 // Se imprimen las celdas con los encabezados.
-                $pdf->cell(46.5, 10, 'Nombre', 1, 0, 'C', 1);
-                $pdf->cell(46.5, 10, 'Tipo', 1, 0, 'C', 1);
-                $pdf->cell(46.5, 10, $pdf->encodeString('Categoría'), 1, 0, 'C', 1);
-                $pdf->cell(46.5, 10, 'Modelo', 1, 1, 'C', 1);
+                $pdf->cell(93, 10, 'Nombre', 1, 0, 'C', 1);
+                $pdf->cell(31, 10, 'Tipo', 1, 0, 'C', 1);
+                $pdf->cell(31, 10, $pdf->encodeString('Categoría'), 1, 0, 'C', 1);
+                $pdf->cell(31, 10, 'Modelo', 1, 1, 'C', 1);
                 // Se establece la fuente para los datos de los productos.
                 $pdf->setFont('Times', '', 11);
                 // Se recorren los registros fila por fila.
                 foreach ($dataProductos as $rowProducto) {
                     // Se imprimen las celdas con los datos de los productos.
-                    $pdf->cell(46.5, 10, $pdf->encodeString($rowProducto['nombreprod']), 1, 0);
-                    $pdf->cell(46.5, 10, $rowProducto['tipoproducto'], 1, 0);
-                    $pdf->cell(46.5, 10, $pdf->encodeString($rowProducto['categoria']), 1, 0);
-                    $pdf->cell(46.5, 10, $pdf->encodeString($rowProducto['modelo']), 1, 1);
+                    $pdf->cell(93, 10, $pdf->encodeString($rowProducto['nombreprod']), 1, 0);
+                    $pdf->cell(31, 10, $rowProducto['tipoproducto'], 1, 0);
+                    $pdf->cell(31, 10, $pdf->encodeString($rowProducto['categoria']), 1, 0);
+                    $pdf->cell(31, 10, $pdf->encodeString($rowProducto['modelo']), 1, 1);
                 }
             } else {
-                $pdf->cell(0, 10, $pdf->encodeString('No hay productos para el país seleccionado'), 1, 1);
+                $pdf->cell(0, 10, $pdf->encodeString('No hay productos para la marca seleccionada'), 1, 1);
             }
             // Se llama implícitamente al método footer() y se envía el documento al navegador web.
             $pdf->output('I', 'productos_marca.pdf');
         } else {
-            print('País inexistente');
+            print('Marca inexistente');
         }
     } else {
-        print('País incorrecta');
+        print('Marca incorrecta');
     }
 } else {
-    print('Debe seleccionar un país');
+    print('Debe seleccionar una marca');
 }
