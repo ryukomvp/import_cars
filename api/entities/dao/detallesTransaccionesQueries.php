2e728d4e923fe28f0d76357479abea5e2b0c274f
Jonathan Marchelli <jonathan.marchelli@gmail.com>
2023-09-12 15:41:02 -0600

    SCRUD de detalles transacciones incompleto

--- api/entities/dao/detallesTransaccionesQueries.php
+++ api/entities/dao/detallesTransaccionesQueries.php
@@ -20,51 +20,42 @@ class DetallesTransaccionQueries
     
     public function buscarRegistros($value)
     {
-        $sql = 'SELECT a.idencatransaccion, a.nocomprobante, a.fechatransac, a.lote, a.npoliza, b.numerobod, c.nombrecajero, a.tipopago, CONCAT(d.codigo, " ", d.nombrecodigo) codigo, l.nombre, u.nombreus, v.nombreprov, o.nombreemp
-                FROM encabezadostransacciones a 
-                INNER JOIN bodegas b ON a.idbodega = b.idbodega
-                INNER JOIN cajeros c ON a.idcajero = c.idcajero
-                INNER JOIN codigostransacciones d ON a.idcodigotransaccion = d.idcodigotransaccion
-                INNER JOIN clientes l ON a.idcliente = l.idcliente
-                INNER JOIN vendedores s ON a.idvendedor = s.idvendedor
-                INNER JOIN usuarios u ON s.idusuario = u.idusuario
-                INNER JOIN proveedores v ON a.idproveedor = v.idproveedor
-                INNER JOIN parametros o ON a.idparametro = o.idparametro
-                WHERE b.numerobod LIKE ? OR c.nombrecajero LIKE ? OR a.tipopago LIKE ? OR d.codigo LIKE ? OR d.nombrecodigo LIKE ? OR l.nombre LIKE ? OR u.nombreus LIKE ? OR v.nombreprov LIKE ? OR o.registro LIKE ? 
-                ORDER BY a.nocomprobante';
+        $sql = 'SELECT a.correlativo, a.cantidad, a.preciounitario, a.ventanosujeta, a.ventaexenta, a.ventaafecta, a.descuento, a.valordescuento, a.sumas, a.subtotal, a.ventatotal, a.iva, a.observaciones, b.numerobod, e.numerobod, c.nombreprod, a.descripcion, d.nocomprobante 
+        FROM detallestransacciones a 
+        INNER JOIN bodegas b ON a.idbodegaentrada = b.idbodega 
+        INNER JOIN bodegas e ON a.idbodegasalida = e.idbodega 
+        INNER JOIN productos c ON a.idproducto = c.idproducto 
+        INNER JOIN encabezadostransacciones d ON a.idencatransaccion = d.idencatransaccion 
+        ORDER BY a.correlativo';
         $params = array("%$value%","%$value%","%$value%","%$value%","%$value%","%$value%","%$value%","%$value%","%$value%");
         return Database::getRows($sql, $params);
     }
 
     public function crearRegistro()
     {
-        $sql = 'INSERT INTO encabezadostransacciones(nocomprobante, fechatransac, lote, npoliza, idbodega, idcajero, tipopago, idcodigotransaccion, idcliente, idvendedor, idproveedor, idparametro)
-                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        $params = array($this->nocomprobante, $this->fechatransac, $this->lote, $this->npoliza, $this->idbodega, $this->idcajero, $this->tipopago, $this->idcodigotransaccion, $this->idcliente, $this->idvendedor, $this->idproveedor, $this->idparametro);
+        $sql = 'INSERT INTO detallestransacciones(correlativo, cantidad, preciounitario, ventanosujeta, ventaexenta, ventaafecta, descuento, valordescuento, sumas, subtotal, ventatotal, iva, observaciones, idbodegaentrada, idbodegasalida, idproducto, descripcion, idencatransaccion)
+                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        $params = array($this->correlativo, $this->cantidad, $this->preciounitario, $this->ventanosujeta, $this->ventaexenta, $this->ventaafecta, $this->descuento, $this->valordescuento, $this->sumas, $this->subtotal, $this->ventatotal, $this->iva, $this->observaciones, $this->idbodegaentrada, $this->idbodegasalida, $this->idproducto, $this->descripcion, $this->idencatransaccion);
         return Database::executeRow($sql, $params);
     }
 
     public function leerRegistros()
     {
-        $sql = 'SELECT a.idencatransaccion, a.nocomprobante, a.fechatransac, a.lote, a.npoliza, b.numerobod, c.nombrecajero, a.tipopago, CONCAT(d.codigo, " ", d.nombrecodigo) codigo, l.nombre, u.nombreus, v.nombreprov, o.nombreemp
-                FROM encabezadostransacciones a 
-                INNER JOIN bodegas b ON a.idbodega = b.idbodega
-                INNER JOIN cajeros c ON a.idcajero = c.idcajero
-                INNER JOIN codigostransacciones d ON a.idcodigotransaccion = d.idcodigotransaccion
-                INNER JOIN clientes l ON a.idcliente = l.idcliente
-                INNER JOIN vendedores s ON a.idvendedor = s.idvendedor
-                INNER JOIN usuarios u ON s.idusuario = u.idusuario
-                INNER JOIN proveedores v ON a.idproveedor = v.idproveedor
-                INNER JOIN parametros o ON a.idparametro = o.idparametro
-                ORDER BY a.nocomprobante';
+        $sql = 'SELECT a.correlativo, a.cantidad, a.preciounitario, a.ventanosujeta, a.ventaexenta, a.ventaafecta, a.descuento, a.valordescuento, a.sumas, a.subtotal, a.ventatotal, a.iva, a.observaciones, b.numerobod, e.numerobod, c.nombreprod, a.descripcion, d.nocomprobante 
+        FROM detallestransacciones a 
+        INNER JOIN bodegas b ON a.idbodegaentrada = b.idbodega 
+        INNER JOIN bodegas e ON a.idbodegasalida = e.idbodega 
+        INNER JOIN productos c ON a.idproducto = c.idproducto 
+        INNER JOIN encabezadostransacciones d ON a.idencatransaccion = d.idencatransaccion 
+        ORDER BY a.correlativo';
         return Database::getRows($sql);
     }
 
     public function leerUnRegistro()
     {
-        $sql = 'SELECT idencatransaccion, nocomprobante, fechatransac, lote, npoliza, idbodega, idcajero, tipopago, idcodigotransaccion, idcliente, idvendedor, idproveedor, idparametro
+        $sql = 'SELECT iddetalletransaccion, correlativo, cantidad, preciounitario, ventanosujeta, ventaexenta, ventaafecta, descuento, valordescuento, sumas, subtotal, ventatotal, iva, observaciones, idbodegaentrada, idbodegasalida, idproducto, descripcion, idencatransaccion
                 FROM encabezadostransacciones
-                WHERE idencatransaccion = ?';
+                WHERE iddetalletransaccion = ?';
         $params = array($this->id);
         return Database::getRow($sql, $params);
     }
@@ -72,23 +63,17 @@ class DetallesTransaccionQueries
     public function actualizarRegistro()
     {
         $sql = 'UPDATE encabezadostransacciones
-                SET nocomprobante = ?, fechatransac = ?, lote = ?, npoliza = ?, idbodega = ?, idcajero = ?, tipopago = ?, idcodigotransaccion = ?, idcliente = ?, idvendedor = ?, idproveedor = ?, idparametro = ?
-                WHERE idencatransaccion = ?';
-        $params = array($this->nocomprobante, $this->fechatransac, $this->lote, $this->npoliza, $this->idbodega, $this->idcajero, $this->tipopago, $this->idcodigotransaccion, $this->idcliente, $this->idvendedor, $this->idproveedor, $this->idparametro, $this->id);
+                SET correlativo = ?, cantidad = ?, preciounitario = ?, ventanosujeta = ?, ventaexenta = ?, ventaafecta = ?, descuento = ?, valordescuento = ?, sumas = ?, subtotal = ?, ventatotal = ?, iva = ?, observaciones = ?, idbodegaentrada = ?, idbodegasalida = ?, idproducto = ?, descripcion = ?, idencatransaccion = ?
+                WHERE iddetalletransaccion = ?';
+        $params = array($this->correlativo, $this->cantidad, $this->preciounitario, $this->ventanosujeta, $this->ventaexenta, $this->ventaafecta, $this->descuento, $this->valordescuento, $this->sumas, $this->subtotal, $this->ventatotal, $this->iva, $this->observaciones, $this->idbodegaentrada, $this->idbodegasalida, $this->idproducto, $this->descripcion, $this->idencatransaccion, $this->id);
         return Database::executeRow($sql, $params);
     }
 
     public function eliminarRegistro()
     {
-        $sql = 'DELETE FROM encabezadostransacciones
-                WHERE idencatransaccion = ?';
+        $sql = 'DELETE FROM detallestransacciones
+                WHERE iddetalletransaccion = ?';
         $params = array($this->id);
         return Database::executeRow($sql, $params);
-    }
-
-    public function leerTiposPagos()
-    {
-        $estados = array(array('Efectivo','Efectivo'), array('Tarjeta','Tarjeta'));
-        return $estados;
     }
 }
