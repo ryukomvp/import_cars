9b9642ee6f19ebddcb941bde2c9eb4ce65180068
kevinrivasd <82895654+kevinrivasd@users.noreply.github.com>
2023-10-02 19:24:06 -0600

    Quite la foto de perfil

--- controllers/account.js
+++ controllers/account.js
@@ -293,14 +293,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                     </div>
                     <span class="text-4xl font-semibold text-white dark:text-white">Import Cars</span>
                     <div class="flex items-center md:order-2">
-                        <button type="button"
-                            class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                            id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
-                            data-dropdown-placement="bottom">
-                            <span class="sr-only">Open user menu</span>
-                            <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg"
-                                alt="user photo">
-                        </button>
+                        
                         <!-- Dropdown menu -->
                         <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
                             id="user-dropdown">
