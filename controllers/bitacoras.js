9c4fb27c6deaafaae1de0f5c67b0a92ca2fdda07
ryukomvp <daniel123hernandez15@gmail.com>
2023-10-12 00:14:44 -0600

    bitacoras

--- controllers/bitacoras.js
+++ controllers/bitacoras.js
@@ -1,21 +1,11 @@
 // Constante para completar la ruta de la API.
-const CATEGORIA_API = 'business/bitacoras.php';
+const BITACORAS_API = 'business/bitacoras.php';
 // Constante para establecer el formulario de buscar.
 const FORMULARIO_BUSQUEDA = document.getElementById('buscarFormulario');
-// Constante para establecer el formulario de guardar.
-const EJECUTAR_FORMULARIO = document.getElementById('ejecutarFormulario');
-// Constante para establecer el título de la modal.
-const TITULO = document.getElementById('titulo');
 // Constantes para establecer el contenido de la tabla.
 const REGISTROS_TABLA = document.getElementById('registros');
-// Constante para capturar el modal.
-const ABRIR_MODAL = new Modal(document.getElementById('abrirModal'));
-// Constante para el texto del boton
-const BTN_ACCION = document.getElementById('accion');
 // Método manejador de eventos para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
-    // se quita el autollenado de los campos del formulario
-    document.getElementById('ejecutarFormulario').autocomplete = 'off';
     // Llamada a la función para llenar la tabla con los registros disponibles.
     cargarRegistros();
 });
@@ -30,29 +20,6 @@ FORMULARIO_BUSQUEDA.addEventListener('submit', (event) => {
     cargarRegistros(FORM);
 });
 
-// Método manejador de eventos para cuando se envía el formulario de guardar.
-EJECUTAR_FORMULARIO.addEventListener('submit', async (event) => {
-    // Se evita recargar la página web después de enviar el formulario.
-    event.preventDefault();
-    // Se verifica la acción a realizar.
-    (document.getElementById('id').value) ? action = 'actualizarRegistro' : action = 'crearRegistro';
-    // Constante tipo objeto con los datos del formulario.
-    const FORM = new FormData(EJECUTAR_FORMULARIO);
-    // Petición para guardar los datos del formulario.
-    const JSON = await dataFetch(CATEGORIA_API, action, FORM);
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-    if (JSON.status) {
-        // Se carga nuevamente la tabla para visualizar los cambios.
-        cargarRegistros();
-        // Se cierra la caja de diálogo.
-        ABRIR_MODAL.hide();
-        // Se muestra un mensaje de éxito.
-        sweetAlert(1, JSON.message, true);
-    } else {
-        sweetAlert(2, JSON.exception, false);
-    }
-});
-
 /*
 *   Función asíncrona para llenar la tabla con los registros disponibles.
 *   Parámetros: form (objeto opcional con los datos de búsqueda).
@@ -64,7 +31,7 @@ async function cargarRegistros(form = null) {
     // Se verifica la acción a realizar.
     (form) ? action = 'buscarRegistros' : action = 'leerRegistros';
     // Petición para obtener los registros disponibles.
-    const JSON = await dataFetch(CATEGORIA_API, action, form);
+    const JSON = await dataFetch(BITACORAS_API, action, form);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (JSON.status) {
         // Se recorre el conjunto de registros fila por fila.
@@ -73,102 +40,12 @@ async function cargarRegistros(form = null) {
             REGISTROS_TABLA.innerHTML += `
                 <tr class="bg-white hover:bg-blue-200">
                     <td class="hidden">${row.idbitacora}</td>
-                    <td>${row.mensaje}</td>
-                    <td>${row.fechabitacora}</td>
-                    <td>
-                        <button onclick="eliminarRegistro(${row.idbitacora})"  
-                        class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mt-2 mb-2"
-                        type="button">
-                            <svg class="w-[30px] h-[30px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/>
-                            </svg>
-                        </button>
-                    </td>
+                    <td class="py-3">${row.mensaje}</td>
+                    <td class="py-3">${row.fechabitacora}</td>
                 </tr>
             `;
         });
     } else {
         sweetAlert(4, JSON.exception, true);
     }
-}
-
-/*
-*   Función para preparar el formulario al momento de insertar un registro.
-*   Parámetros: ninguno.
-*   Retorno: ninguno.
-*/
-function crearRegistro() {
-    // Se abre la caja de diálogo que contiene el formulario.
-    ABRIR_MODAL.show();
-    // Se restauran los elementos del formulario.
-    EJECUTAR_FORMULARIO.reset();
-    // Texto del boton para crear un registro
-    BTN_ACCION.textContent = 'Añadir';
-    // Se asigna título a la caja de diálogo.
-    TITULO.textContent = 'Crear categoria';
-}
-
-/*
-*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
-*   Parámetros: id (identificador del registro seleccionado).
-*   Retorno: ninguno.
-*/
-async function actualizarRegistro(id) {
-    // Se define una constante tipo objeto con los datos del registro seleccionado.
-    const FORM = new FormData();
-    FORM.append('id', id);
-    // Petición para obtener los datos del registro solicitado.
-    const JSON = await dataFetch(CATEGORIA_API, 'leerUnRegistro', FORM);
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-    if (JSON.status) {
-        // Se abre la caja de diálogo que contiene el formulario.
-        ABRIR_MODAL.show();
-        // Se restauran los elementos del formulario.
-        EJECUTAR_FORMULARIO.reset();
-        // Texto del boton para crear un registro
-        BTN_ACCION.textContent = 'Actualizar';
-        // Se asigna título para la caja de diálogo.
-        TITULO.textContent = 'Actualizar categoria';
-        // Se inicializan los campos del formulario.
-        document.getElementById('id').value = JSON.dataset.idcategoria;
-        document.getElementById('categoria').value = JSON.dataset.categoria;
-    } else {
-        sweetAlert(2, JSON.exception, false);
-    }
-}
-
-/*
-*   Función asíncrona para eliminar un registro.
-*   Parámetros: id (identificador del registro seleccionado).
-*   Retorno: ninguno.
-*/
-async function eliminarRegistro(id) {
-    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
-    const RESPONSE = await confirmAction('¿Desea eliminar la categoria de forma permanente?');
-    // Se verifica la respuesta del mensaje.
-    if (RESPONSE) {
-        // Se define una constante tipo objeto con los datos del registro seleccionado.
-        const FORM = new FormData();
-        FORM.append('idcategoria', id);
-        // Petición para eliminar el registro seleccionado.
-        const JSON = await dataFetch(CATEGORIA_API, 'eliminarRegistro', FORM);
-        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-        if (JSON.status) {
-            // Se carga nuevamente la tabla para visualizar los cambios.
-            cargarRegistros();
-            // Se muestra un mensaje de éxito.
-            sweetAlert(1, JSON.message, true);
-        } else {
-            sweetAlert(2, JSON.exception, false);
-        }
-    }
-}
-
-function openReport(id) {
-    // Se declara una constante tipo objeto con la ruta específica del reporte en el servidor.
-    const PATH = new URL(`${SERVER_URL}reports/productosPorCategoria.php`);
-    // Se agrega un parámetro a la ruta con el valor del registro seleccionado.
-    PATH.searchParams.append('idcategoria', id);
-    // Se abre el reporte en una nueva pestaña del navegador web.
-    window.open(PATH.href);
 }
