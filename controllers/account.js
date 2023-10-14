9267ed7bbd40e64320a03615dfa06aa0d88bb953
kevinrivasd <82895654+kevinrivasd@users.noreply.github.com>
2023-10-14 09:23:21 -0600

    Quitamos un boton de el menu

--- controllers/account.js
+++ controllers/account.js
@@ -146,13 +146,10 @@ document.addEventListener('DOMContentLoaded', async () => {
                                         class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Encabezados transaccion</a>
                                 </li>
                                 <li class="$hidden">
-                                    <a href="ventaProducto.html"
+                                    <a href="../views/transaccionesVentas.html"
                                         class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Ventas</a>
                                 </li>
-                                <li class="hidden">
-                                    <a href="detallesTransacciones.html"
-                                        class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Detalles transaccion</a>
-                                </li>
+                                
                                 <li class="hidden">
                                     <a href="codigosTransacciones.html"
                                         class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Codigos transaccion</a>
@@ -245,18 +242,6 @@ document.addEventListener('DOMContentLoaded', async () => {
                                         class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Bitacoras</a>
                                 </li>
                             </ul>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
-                                </svg>
-                                <span class="flex-1 ml-3 whitespace-nowrap">Procesos</span>
-                            </a>
                         </li>
                         <li>
                             <a href="reportes.html"
